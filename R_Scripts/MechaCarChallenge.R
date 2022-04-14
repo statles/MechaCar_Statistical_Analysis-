04e886e537e6fa91226a93e6c3b9fad6eb429e3c
statles <alex.statley@gmail.com>
2022-04-13 21:18:28 -0700

    adding deliverable 2

--- R_Scripts/MechaCarChallenge.R
+++ R_Scripts/MechaCarChallenge.R
@@ -5,4 +5,12 @@ mechaCar_mgp <- read.csv('../MechaCar_mpg.csv', check.names=F, stringsAsFactors 
 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechaCar_mgp) #generate multiple linear regression model
 
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechaCar_mgp))
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechaCar_mgp)) #summary table of lm
+
+mechaCar_suspension <- read.csv('../Suspension_Coil.csv', check.names=F, stringsAsFactors = F) #import suspension coil data
+
+total_summary < - mechaCar_suspension %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) #summary of PSI --Doesnt work right now--
+
+lot_summary <- mechaCar_suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))#summary of PSI by lots
+
+?summary()
